932e1bdbf0a1977c7b8c6ca6c2f22857422081a7
LuisIrua23 <lairua@soy.sena.edu.co>
2024-07-29 21:58:49 -0500

    epa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,15 +85,15 @@
                     @else
                         {{-- despliegue --}}
                     {{-- <div class="collapse navbar-collapse" id="navbarNavDropdown"> --}}
-
+                        
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a href="profile" class="dropdown-item">perfil</a>
-                            <a href="" class="dropdown-item">otra parte</a>
+                                {{-- <a href="profile" class="dropdown-item">perfil</a>
+                            <a href="" class="dropdown-item">otra parte</a> --}}
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
